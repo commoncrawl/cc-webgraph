1a211605ed891e31ea15a4b33ce8a0cc41b2a007
Sebastian Nagel <snagel@apache.org>
2017-11-21 13:17:14 +0100

    Roll-back to crawler-commons 0.9 from development version

--- src/main/java/org/commoncrawl/webgraph/HostToDomainGraph.java
+++ src/main/java/org/commoncrawl/webgraph/HostToDomainGraph.java
@@ -77,7 +77,7 @@ public class HostToDomainGraph {
 		long id = Long.parseLong(line.substring(0, sep));
 		String revHost = line.substring(sep+1);
 		String host = reverseHost(revHost);
-		String domain = EffectiveTldFinder.getAssignedDomain(host, true, true);
+		String domain = EffectiveTldFinder.getAssignedDomain(host, true);
 		if (domain == null) {
 			setValue(id, -1);
 			return null;
@@ -142,6 +142,8 @@ public class HostToDomainGraph {
 			LOG.error("Invalid number: " + args[0]);
 			System.exit(1);
 		}
+		// exclude private domains / suffixes
+		EffectiveTldFinder.getInstance().initialize(true);
 		HostToDomainGraph converter;
 		if (maxSize <= Arrays.MAX_ARRAY_SIZE) {
 			converter = new HostToDomainGraph((int) maxSize);
