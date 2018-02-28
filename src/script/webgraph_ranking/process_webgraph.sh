6687b2eb081572f9687406238971ade47187934d
Sebastian Nagel <snagel@apache.org>
2018-02-28 15:50:12 +0100

    Improve sort configuration, expose options as variables

--- src/script/webgraph_ranking/process_webgraph.sh
+++ src/script/webgraph_ranking/process_webgraph.sh
@@ -49,6 +49,11 @@ STOP_FILE_=$LOGDIR/$(basename $0 .sh).stop
 
 VERTICES_FIELDS=${VERTICES_FIELDS:-2}
 
+# threads and buffer size used for sorting
+export SORT_THREADS=$((($THREADS > 4) ? ($THREADS/2) : 2))
+# take 10% of main memory, at least 1 GB, for sort "chunks"
+export SORT_BUFFER_SIZE=$(free -g | perl -ne 'do { print 1+int($1*.1), "g"; last } if /(\d+)/')
+
 
 function join_rank() (
     set -exo pipefail
@@ -56,14 +61,11 @@ function join_rank() (
     _IN=$2
     _VERT=$3
     _OUT=$4
-    _SORT_THREADS=${5:-2}
     _EXTRA_FIELDS=""
-    if [ -n "$6" ]; then
-        _EXTRA_FIELDS=",$6"
+    if [ -n "$5" ]; then
+        _EXTRA_FIELDS=",$5"
     fi
 
-    _SORT_BUFFER=16g
-    _SORT_COMPRESS="--compress-program=gzip"
 
     if [ -d $_VERT ]; then
         # _VERT is a directory with multiple vertices files
@@ -74,23 +76,22 @@ function join_rank() (
     ### assign ranks on sorted lines by nl
 	$LW it.unimi.dsi.law.io.tool.DataInput2Text --type $_DATA_TYPE $_IN - \
         | paste - <(zcat $_VERT | cut -f2$_EXTRA_FIELDS) \
-        | sort --parallel $_SORT_THREADS --batch-size $(($_SORT_THREADS*4)) --buffer-size $_SORT_BUFFER $_SORT_COMPRESS -t$'\t' -k1,1gr --stable \
+        | sort --parallel $SORT_THREADS --batch-size $(($SORT_THREADS*4)) --buffer-size=$SORT_BUFFER_SIZE --compress-program=gzip -t$'\t' -k1,1gr --stable \
         | nl -w1 -nln \
         | gzip >$_OUT
 )
 
 function join_harmonicc_pagerank() {
     NAME="$1"
-    _SORT_THREADS="$2"
-    _IN_HC="$3"
-    _IN_PR="$4"
-    _OUT="$5"
+    _IN_HC="$2"
+    _IN_PR="$3"
+    _OUT="$4"
     _EXTRA_FIELDS=""
-    if [ -n "$6" ]; then
-        _EXTRA_FIELDS=",$6"
-        _EXTRA_FIELDS_HEADER="\t$7"
+    if [ -n "$5" ]; then
+        _EXTRA_FIELDS=",$5"
+        _EXTRA_FIELDS_HEADER="\t$6"
     fi
-    SORTOPTS="--parallel=$_SORT_THREADS --batch-size=$(($_SORT_THREADS*4)) --buffer-size=16g --compress-program=gzip"
+    SORTOPTS="--parallel=$SORT_THREADS --batch-size=$(($SORT_THREADS*4)) --buffer-size=$SORT_BUFFER_SIZE --compress-program=gzip"
     (echo -e "#harmonicc_pos\t#harmonicc_val\t#pr_pos\t#pr_val\t#host_rev$_EXTRA_FIELDS_HEADER";
      zcat $_IN_HC | sort $SORTOPTS -t$'\t' -k3,3 --unique --stable \
          | join -a1 -a2 -e'---' -t$'\t' -j3 -o1.1,1.2,2.1,2.2,0$_EXTRA_FIELDS - \
@@ -103,8 +104,7 @@ function connected_distrib() {
     NUM_NODES=$1
     INPUT=$2
     OUTPUT=$3
-    _SORT_THREADS=${5:-2}
-    SORTOPTS="--parallel=$_SORT_THREADS --batch-size=$(($_SORT_THREADS*4)) --buffer-size=16g --compress-program=gzip"
+    SORTOPTS="--parallel=$SORT_THREADS --batch-size=$(($SORT_THREADS*4)) --buffer-size=$SORT_BUFFER_SIZE --compress-program=gzip"
     (echo -e "  #freq #size"; \
      $LW it.unimi.dsi.law.io.tool.DataInput2Text --type int $INPUT - | sort $SORTOPTS -nr | uniq -c \
          | perl -lpe 'if ($. <= 10) { /(\d+)$/; $_ .= sprintf("\t%2.2f%%", 100*$1/'$NUM_NODES') }') \
@@ -190,7 +190,6 @@ _step_bg connected 15 \
 _step_bg strongly-connected 15 \
          $WG $WGP.algo.StronglyConnectedComponents --renumber --sizes $FULLNAME
 
-SORT_THREADS=$((($THREADS > 4) ? ($THREADS/2) : 2))
 EXTRA_FIELDS=""
 EXTRA_FIELDS_JOIN=""
 if [ $VERTICES_FIELDS -gt 2 ]; then
@@ -201,27 +200,26 @@ if [ $VERTICES_FIELDS -gt 2 ]; then
     done
 fi
 _step_bg join_harmonicc 15 \
-         join_rank float $FULLNAME-harmonicc.bin $VERTICES $FULLNAME-harmonic-centrality.txt.gz $SORT_THREADS "$EXTRA_FIELDS"
+         join_rank float $FULLNAME-harmonicc.bin $VERTICES $FULLNAME-harmonic-centrality.txt.gz "$EXTRA_FIELDS"
 _step_bg join_pr_gs 15 \
-         join_rank double $FULLNAME-pagerank.ranks $VERTICES $FULLNAME-pagerank.txt.gz $SORT_THREADS
+         join_rank double $FULLNAME-pagerank.ranks $VERTICES $FULLNAME-pagerank.txt.gz
 
 wait # until background processes are finished
 
-# stats use connected components files, wait for these to be finished
-_step_bg stats 60 \
-         $WG $WGP.Stats --save-degrees $FULLNAME
-
 # join ranks into one file
 _step_bg join_harmonicc_pagerank 60 \
-         join_harmonicc_pagerank $NAME $SORT_THREADS $FULLNAME-harmonic-centrality.txt.gz $FULLNAME-pagerank.txt.gz $FULLNAME-ranks.txt.gz "$EXTRA_FIELDS_JOIN" "#n_hosts"
+         join_harmonicc_pagerank $NAME $FULLNAME-harmonic-centrality.txt.gz $FULLNAME-pagerank.txt.gz $FULLNAME-ranks.txt.gz "$EXTRA_FIELDS_JOIN" "#n_hosts"
 
+# stats use connected components files, wait for these to be finished
+_step stats \
+         $WG $WGP.Stats --save-degrees $FULLNAME
 
 NODES=$(perl -lne 'print if s@^nodes=@@' $FULLNAME.stats)
 _step connected_distrib \
-      connected_distrib $NODES $FULLNAME.wccsizes $FULLNAME-connected-components-distrib.txt.gz $SORT_THREADS
+      connected_distrib $NODES $FULLNAME.wccsizes $FULLNAME-connected-components-distrib.txt.gz
 # TODO: should use *.sccdistr
 _step strongly_connected_distrib \
-      connected_distrib $NODES $FULLNAME.sccsizes $FULLNAME-strongly-connected-components-distrib.txt.gz $SORT_THREADS
+      connected_distrib $NODES $FULLNAME.sccsizes $FULLNAME-strongly-connected-components-distrib.txt.gz
 
 _step indegree_distrib \
       degree_distrib indegree $FULLNAME outdegree_distrib \
