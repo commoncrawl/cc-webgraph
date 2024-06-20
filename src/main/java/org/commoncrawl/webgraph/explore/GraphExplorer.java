ace69c317e9db6fa38fbf0848f25a6927ea9257b
Sebastian Nagel <sebastian@commoncrawl.org>
2024-06-20 16:26:39 +0200

    feat: tool and scripts to interactively explore webgraph
    - complete Javadoc
    - add methods to get a common subset (intersection) or the union
      of the successors or predecessors of a list of vertices
    - add class to count occurrences of integers given a list of iterators
      as input
    - more methods to print vertices

--- src/main/java/org/commoncrawl/webgraph/explore/GraphExplorer.java
+++ src/main/java/org/commoncrawl/webgraph/explore/GraphExplorer.java
@@ -1,24 +1,34 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
- * Copyright (C) 2022 Common Crawl and contributors
+ * Copyright (C) 2024 Common Crawl and contributors
  */
 package org.commoncrawl.webgraph.explore;
 
+import java.io.IOException;
+
+import org.commoncrawl.webgraph.CountingMergedIntIterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import it.unimi.dsi.fastutil.io.BinIO;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.util.ImmutableExternalPrefixMap;
 import it.unimi.dsi.webgraph.ImmutableGraph;
 import it.unimi.dsi.webgraph.LazyIntIterator;
 
-import java.io.IOException;
-
-import it.unimi.dsi.fastutil.io.BinIO;
-import it.unimi.dsi.util.ImmutableExternalPrefixMap;
-
+/**
+ * Utility class for graph exploration: load and hold all required web graph
+ * data structures, provided methods to interactively explore the graph.
+ */
 public class GraphExplorer {
 
+	private static Logger LOG = LoggerFactory.getLogger(GraphExplorer.class);
+
 	public class Graph {
-		String name;
-		ImmutableGraph graph;
-		ImmutableGraph graphT;
-		ImmutableExternalPrefixMap vertexMap;
+		public String name;
+		public ImmutableGraph graph;
+		public ImmutableGraph graphT;
+		public ImmutableExternalPrefixMap vertexMap;
 
 		public Graph(String name) {
 			this.name = name;
@@ -27,8 +37,7 @@ public class GraphExplorer {
 				graphT = ImmutableGraph.loadMapped(name + "-t");
 				vertexMap = (ImmutableExternalPrefixMap) BinIO.loadObject(name + ".iepm");
 			} catch (IOException | ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				LOG.error("Failed to load graph {}:", name, e);
 			}
 			// TODO: fall-back: instead of .iepm load .mph .fcl .smph
 		}
@@ -85,8 +94,16 @@ public class GraphExplorer {
 		return new Vertex(vertexLabel);
 	}
 
+	public Vertex getVertex(long vertexId) {
+		return new Vertex(vertexId);
+	}
+
 	public void setVertex(String vertexLabel) {
 		v = getVertex(vertexLabel);
+	}
+
+	public void setVertex(long vertexId) {
+		v = getVertex(vertexId);
 	}
 
 	public int outdegree() {
@@ -108,50 +125,156 @@ public class GraphExplorer {
 		return g.graphT.outdegree((int) g.vertexMap.getLong(vertexLabel));
 	}
 
-	/* reimplementation of commands provided by pywebgraph (cn, pwn, ls, sl) */
-	public void cn(String nodeLabel) {
-		setVertex(nodeLabel);
+	public long[] commonPredecessors(long[] vertices) {
+		return commonPredecessors(vertices, vertices.length);
+	}
+
+	public long[] commonPredecessors(long[] vertices, int minCommon) {
+		return commonSuccessors(g.graphT, vertices, minCommon);
+	}
+
+	public long[] commonSuccessors(long[] vertices) {
+		return commonSuccessors(vertices, vertices.length);
+	}
+
+	public long[] commonSuccessors(long[] vertices, int minCommon) {
+		return commonSuccessors(g.graph, vertices, minCommon);
+	}
+
+	public long[] commonSuccessors(ImmutableGraph graph, long[] vertices, int minCommon) {
+		LazyIntIterator[] iters = new LazyIntIterator[vertices.length];
+		for (int i = 0; i < vertices.length; i++) {
+			iters[i] = graph.successors((int) vertices[i]);
+		}
+		CountingMergedIntIterator iter = new CountingMergedIntIterator(iters);
+		LongArrayList res = new LongArrayList();
+		int id;
+		while (iter.hasNext()) {
+			id = iter.nextInt();
+			if (iter.getCount() >= minCommon) {
+				res.add(id);
+			}
+		}
+		res.trim();
+		return res.elements();
+	}
+
+	/* Reimplementation of commands provided by pywebgraph (cn, pwn, ls, sl) */
+
+	/**
+	 * Change the current working node / vertex.
+	 * 
+	 * @param vertexLabel vertex label (node name)
+	 */
+	public void cn(String vertexLabel) {
+		setVertex(vertexLabel);
 		pwn();
 	}
 
+	/**
+	 * Change the current working node / vertex.
+	 * 
+	 * @param vertexId vertex ID
+	 */
+	public void cn(long vertexId) {
+		setVertex(vertexId);
+		pwn();
+	}
+
+	/**
+	 * Print the current working node / vertex.
+	 */
 	public void pwn() {
+		if (v == null) {
+			throw new NullPointerException("Current orking node not set, use cn(...) to define the working node.");
+		}
 		print(v.toString());
 	}
 
+	/**
+	 * Print the successors (outgoing links) of the current working node / vertex.
+	 */
 	public void ls() {
+		if (v == null) {
+			throw new NullPointerException("Current orking node not set, use cn(...) to define the working node.");
+		}
 		ls(v.id);
 	}
+
+	/**
+	 * Print the successors (outgoing links) of a vertex.
+	 * 
+	 * @param vertexId vertex ID
+	 */
 	public void ls(long vertexId) {
 		printVertices(g.graph.successors((int) vertexId));
 	}
+
+	/**
+	 * Print the successors (outgoing links) of a vertex.
+	 * 
+	 * @param vertexLabel vertex label / vertex name
+	 */
 	public void ls(String vertexLabel) {
 		ls(g.vertexMap.getLong(vertexLabel));
 	}
 
+	/**
+	 * Print the predecessors (incoming links) of the current working node / vertex.
+	 */
 	public void sl() {
+		if (v == null) {
+			throw new NullPointerException("Current orking node not set, use cn(...) to define the working node.");
+		}
 		sl(v.id);
 	}
+
+	/**
+	 * Print the predecessors (incoming links) of a vertex.
+	 * 
+	 * @param vertexId vertex ID
+	 */
 	public void sl(long vertexId) {
 		printVertices(g.graphT.successors((int) vertexId));
 	}
+
+	/**
+	 * Print the predecessors (incoming links) of a vertex.
+	 * 
+	 * @param vertexLabel vertex label / vertex name
+	 */
 	public void sl(String vertexLabel) {
 		sl(g.vertexMap.getLong(vertexLabel));
 	}
 
-	/* utilities */
+	/* Utilities */
+
 	private void print(String s) {
 		System.out.println(s);
 	}
-	private void print(int s) {
-		System.out.println(s);
-	}
 
-	private void printVertices(LazyIntIterator it) {
+	public void printVertices(LazyIntIterator it) {
 		int next = it.nextInt();
 		int i = 0;
-		while (next != -1) {
+		while (next != CountingMergedIntIterator.EMPTY_INPUT_ITERATOR_VALUE) {
 			print(String.format("%d: %s", i, (new Vertex(next)).toString()));
 			next = it.nextInt();
+			i++;
+		}
+	}
+
+	public void printVertices(long[] vertexIDs) {
+		int i = 0;
+		for (long id : vertexIDs) {
+			print(String.format("%d: %s", i, (new Vertex(id)).toString()));
+			i++;
+		}
+	}
+
+	public void printVertices(int[] vertexIDs) {
+		int i = 0;
+		for (long id : vertexIDs) {
+			print(String.format("%d: %s", i, (new Vertex(id)).toString()));
 			i++;
 		}
 	}
