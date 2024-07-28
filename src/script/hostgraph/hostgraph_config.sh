90b8f0811db1a68441479dc0e53477be7072193f
Thom Vaughan <thom@commoncrawl.org>
2024-07-28 21:37:54 +0000

    chore: update hostgraph configuration for cc-main-2024-apr-jun=jul

--- src/script/hostgraph/hostgraph_config.sh
+++ src/script/hostgraph/hostgraph_config.sh
@@ -12,7 +12,7 @@
 ### saved as tuples <from_host, to_host>
 
 # crawls to be processed
-CRAWLS=("CC-MAIN-2024-10" "CC-MAIN-2024-18" "CC-MAIN-2024-22")
+CRAWLS=("CC-MAIN-2024-22" "CC-MAIN-2024-26" "CC-MAIN-2024-30")
 
 INPUT_BASE_URL="s3://commoncrawl/"
 
@@ -42,7 +42,7 @@ S3A_OUTPUT_PREFIX=s3a://commoncrawl-webgraph
 ################################################################################
 # construct a merged graph of multiple monthly crawls
 
-MERGE_NAME=cc-main-2024-feb-apr-may
+MERGE_NAME=cc-main-2024-may-jun-jul
 
 # Naming convention should be the three months' crawls that are
 # used to generate this graph release. In the event of multiple months
